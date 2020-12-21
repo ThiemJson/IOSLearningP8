657683464cd08f7d6e7d9be4e937094666c85d15
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-12-21 23:05:16 +0700

    Initial Commit

---
+++ IOSLearningP8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSLearningP8
+//
+//  Created by Thiem Jason on 12/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOSLearningP8/IOSLearningP8App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSLearningP8App.swift
+//  IOSLearningP8
+//
+//  Created by Thiem Jason on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct IOSLearningP8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
