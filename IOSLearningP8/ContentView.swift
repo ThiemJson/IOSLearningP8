737909e6c9efb23db5dc253bf8b6c44a9b8d77b9
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-12-22 22:17:11 +0700

    End

--- IOSLearningP8/ContentView.swift
+++ IOSLearningP8/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city" : "Nashville",
+                }
+            }
+            """
+
+            // more code to come
+            let data = Data(input.utf8)
+            if let user = try? JSONDecoder().decode(User.self, from: data) {
+                print(user.address.street)
+            }
+            else{
+                print("==> Decode fail")
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
 }
